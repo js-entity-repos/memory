cc504b6e77dc8ef0e77eda619929b0d39b29ef3b
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-03-28 09:05:05 +0100

    fix: Fixes cursors and updates core to ^8.0.0. (#15)

--- src/functions/getEntities.ts
+++ src/functions/getEntities.ts
@@ -1,12 +1,13 @@
 import GetEntities from '@js-entity-repos/core/dist/signatures/GetEntities';
+import { end } from '@js-entity-repos/core/dist/types/Cursor';
 import Entity from '@js-entity-repos/core/dist/types/Entity';
 import Pagination from '@js-entity-repos/core/dist/types/Pagination';
 import { forward } from '@js-entity-repos/core/dist/types/PaginationDirection';
 import Sort from '@js-entity-repos/core/dist/types/Sort';
 import { asc } from '@js-entity-repos/core/dist/types/SortOrder';
-import createCursorFromEntity from '@js-entity-repos/core/dist/utils/createCursorFromEntity';
+import createEndCursorResult from '@js-entity-repos/core/dist/utils/createEndCursorResult';
+import createGetEntitiesResult from '@js-entity-repos/core/dist/utils/createGetEntitiesResult';
 import createPaginationFilter from '@js-entity-repos/core/dist/utils/createPaginationFilter';
-import { first, last } from 'lodash';
 import FacadeConfig from '../FacadeConfig';
 import filterEntities from '../utils/filterEntities';
 import sortEntities from '../utils/sortEntities';
@@ -19,16 +20,19 @@ export default <E extends Entity>(config: FacadeConfig<E>): GetEntities<E> => {
   };
   const defaultSort = { id: asc } as Sort<E>;
   return async ({ filter = {}, sort = defaultSort, pagination = defaultPagination }) => {
+    if (pagination.cursor === end) {
+      return createEndCursorResult(pagination);
+    }
+
     const paginationFilter = createPaginationFilter(pagination, sort);
     const fullFilter = { $and: [filter, paginationFilter] };
     const storedEntities = config.getEntities();
     const matchedEntities = filterEntities({ entities: storedEntities, filter: fullFilter });
     const sortedEntities = sortEntities({ entities: matchedEntities, sort });
-    const start = pagination.direction === forward ? 0 : 0 - pagination.limit;
-    const end = pagination.direction === forward ? pagination.limit : undefined;
-    const entities = sortedEntities.slice(start, end);
-    const nextCursor = createCursorFromEntity(last(entities), sort);
-    const previousCursor = createCursorFromEntity(first(entities), sort);
-    return { entities, nextCursor, previousCursor };
+    const sliceStart = pagination.direction === forward ? 0 : 0 - pagination.limit;
+    const sliceEnd = pagination.direction === forward ? pagination.limit : undefined;
+    const entities = sortedEntities.slice(sliceStart, sliceEnd);
+    const isEnd = sortedEntities.length <= pagination.limit;
+    return createGetEntitiesResult({ entities, isEnd, pagination, sort });
   };
 };
